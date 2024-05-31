35c4a63e79ee5520efc7e64b3a62d00fa1f9f6c9
giovanni <giovanni_ge@hotmai.com>
2024-05-31 18:44:49 -0300

    feat: adicionando codigo no projeto

--- nodejs-api-01/src/app.js
+++ nodejs-api-01/src/app.js
@@ -1,13 +1,10 @@
-const express = require('express');
+import express from 'express';
+
 const app = express();
-const port = 3000;
 
 //criar rota padrão ou raiz
 app.get('/', (req, res) => {
   res.send('Olá Mundo, Node js...');
 });
 
-// excutar a porta 3000
-app.listen(port, () => {
-  console.log(`Servidor rodando no endereço http://localhost:${port}`);
-});
+export default app;
