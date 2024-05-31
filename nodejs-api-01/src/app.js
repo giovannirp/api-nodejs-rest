96911c47ed91fd42ab07e236e1f93bc997632266
giovanni <giovanni_ge@hotmai.com>
2024-05-31 19:37:19 -0300

    Alterando o isonia e alterando codigo

--- nodejs-api-01/src/app.js
+++ nodejs-api-01/src/app.js
@@ -2,12 +2,15 @@ import express from 'express';
 
 const app = express();
 
+// indicar para express ler body com json
+app.use(express.json());
+
 // mock
 const selecoes = [
   {id: 1, selecoes: 'Brasil', grupo: 'G'},
-  {id: 1, selecoes: 'Peru', grupo: 'B'},
-  {id: 1, selecoes: 'Ilha de Salomão', grupo: 'F'},
-  {id: 1, selecoes: 'Thaiti', grupo: 'A'}
+  {id: 2, selecoes: 'Peru', grupo: 'B'},
+  {id: 3, selecoes: 'Ilha de Salomão', grupo: 'F'},
+  {id: 4, selecoes: 'Thaiti', grupo: 'A'}
 ];
 //criar rota padrão ou raiz
 // criando endPoint
@@ -17,6 +20,12 @@ app.get('/', (req, res) => {
 
 app.get('/selecoes', (req, res) => {
   res.status(200).send(selecoes);
+});
+
+// Criando post para cadastrar
+app.post('/selecoes', (req, res) => {
+  selecoes.push(req.body);
+  res.status(201).send('Seleção cadastrando com sucesso!');
 })
 
 export default app;
