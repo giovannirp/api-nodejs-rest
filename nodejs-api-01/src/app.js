e6bcaa83e21fe0e174978067ff1f023199871d80
giovanni <giovanni_ge@hotmai.com>
2024-05-31 19:09:07 -0300

    feat: adicionando dados em json e seprando arquivos

--- nodejs-api-01/src/app.js
+++ nodejs-api-01/src/app.js
@@ -2,9 +2,21 @@ import express from 'express';
 
 const app = express();
 
+// mock
+const selecoes = [
+  {id: 1, selecoes: 'Brasil', grupo: 'G'},
+  {id: 1, selecoes: 'Peru', grupo: 'B'},
+  {id: 1, selecoes: 'Ilha de Salomão', grupo: 'F'},
+  {id: 1, selecoes: 'Thaiti', grupo: 'A'}
+];
 //criar rota padrão ou raiz
+// criando endPoint
 app.get('/', (req, res) => {
   res.send('Olá Mundo, Node js...');
 });
+
+app.get('/selecoes', (req, res) => {
+  res.status(200).send(selecoes);
+})
 
 export default app;
