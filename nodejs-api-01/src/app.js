c3d58cce8845c8e51b91e9c61aa764b8cffd1f95
giovanni <giovanni_ge@hotmai.com>
2024-05-31 20:55:43 -0300

    feat: deletando e criando funções auxiliares

--- nodejs-api-01/src/app.js
+++ nodejs-api-01/src/app.js
@@ -12,6 +12,19 @@ const selecoes = [
   {id: 3, selecoes: 'Ilha de Salomão', grupo: 'F'},
   {id: 4, selecoes: 'Thaiti', grupo: 'A'}
 ];
+
+// função auxiliar
+
+// retornar o objeto por id
+function buscarSelecaoPorId(id) {
+  return selecoes.filter((selecao) => selecao.id == id);
+}
+
+// pegar a posicao ou index do elemento do array por id
+function buscarIdSelecao(id) {
+  return selecoes.findIndex(selecao => selecao.id == id);
+}
+
 //criar rota padrão ou raiz
 // criando endPoint
 app.get('/', (req, res) => {
@@ -22,10 +35,23 @@ app.get('/selecoes', (req, res) => {
   res.status(200).send(selecoes);
 });
 
+// Buscando por ID
+app.get('/selecoes/:id', (req, res) => {
+  let index = req.params.id;
+  
+  res.json(buscarSelecaoPorId(index));
+})
+
 // Criando post para cadastrar
 app.post('/selecoes', (req, res) => {
   selecoes.push(req.body);
   res.status(201).send('Seleção cadastrando com sucesso!');
+});
+
+app.delete('/selecoes/:id', (req, res) => {
+  let index = buscarIdSelecao(req.params.id);
+  selecoes.splice(index, 1);
+  res.send(`Seleção com id ${req.params.id} excluida com sucesso!`);
 })
 
 export default app;
