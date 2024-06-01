4fdc65bf9c46e024ac4405dc84318f1b414cacef
giovanni <giovanni_ge@hotmai.com>
2024-06-01 17:03:10 -0300

    feat: adicionando configuraçao de banco de dados

--- nodejs-api-01/server.js
+++ nodejs-api-01/server.js
@@ -1,8 +1,19 @@
-import app from './src/app.js';
+import app from "./src/app.js";
+
+import conexao from "./infra/conexao.js";
 
 const PORT = 3000;
 
-// excutar a porta 3000
-app.listen(PORT, () => {
-  console.log(`Servidor rodando no endereço http://localhost:${PORT}`);
+// fazer a conexao
+conexao.connect((erro) => {
+  if (erro) {
+    console.log(erro);
+  } else {
+    console.log("Conexão realizada com sucesso!");
+
+    // excutar a porta 3000
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando no endereço http://localhost:${PORT}`);
+    });
+  }
 });
