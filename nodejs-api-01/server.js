adcde55cb5d6c1f2dc5d64ab01fa09347f3df75c
giovanni <giovanni_ge@hotmai.com>
2024-06-01 21:30:21 -0300

    feat: adicionando novos projetos

--- nodejs-api-01/server.js
+++ nodejs-api-01/server.js
@@ -1,19 +1,8 @@
 import app from "./src/app.js";
 
-import conexao from "./infra/conexao.js";
-
 const PORT = 3000;
 
-// fazer a conexao
-conexao.connect((erro) => {
-  if (erro) {
-    console.log(erro);
-  } else {
-    console.log("Conexão realizada com sucesso!");
-
-    // excutar a porta 3000
-    app.listen(PORT, () => {
-      console.log(`Servidor rodando no endereço http://localhost:${PORT}`);
-    });
-  }
+// excutar a porta 3000
+app.listen(PORT, () => {
+  console.log(`Servidor rodando no endereço http://localhost:${PORT}`);
 });
